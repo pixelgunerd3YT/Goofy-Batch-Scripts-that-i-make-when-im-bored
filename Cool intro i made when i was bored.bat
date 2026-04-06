a6492f365de4e0987652aa7db52a51dea4819c34
Mr ZRX <158084979+pixelgunerd3YT@users.noreply.github.com>
2026-04-06 10:00:56 +0200

    Create Cool intro i made when i was bored.bat

---
+++ Cool intro i made when i was bored.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal enabledelayedexpansion
+chcp 65001 >nul
+echo.
+powershell -command "Write-Host '  ________   ________  _________        ________          ___      ___ ___  ________  _______   ________     ' -ForegroundColor Red"
+powershell -command "Write-Host '|\   ___  \|\   __  \|\___   ___\     |\   __  \        |\  \    /  /|\  \|\   __  \|\  ___ \ |\   __  \    ' -ForegroundColor Red"
+powershell -command "Write-Host '\ \  \\ \  \ \  \|\  \|___ \  \_|     \ \  \|\  \       \ \  \  /  / | \  \ \  \|\  \ \   __/|\ \  \|\  \   ' -ForegroundColor Red"
+powershell -command "Write-Host ' \ \  \\ \  \ \  \\\  \   \ \  \       \ \   __  \       \ \  \/  / / \ \  \ \   ____\ \  \_|/_\ \   _  _\  ' -ForegroundColor Red"
+powershell -command "Write-Host '  \ \  \\ \  \ \  \\\  \   \ \  \       \ \  \ \  \       \ \    / /   \ \  \ \  \___|\ \  \_|\ \ \  \\  \| ' -ForegroundColor Red"
+powershell -command "Write-Host '   \ \__\\ \__\ \_______\   \ \__\       \ \__\ \__\       \ \__/ /     \ \__\ \__\    \ \_______\ \__\\ _\ ' -ForegroundColor Red"
+powershell -command "Write-Host '    \|__| \|__|\|_______|    \|__|        \|__|\|__|        \|__|/       \|__|\|__|     \|_______|\|__|\|__| ' -ForegroundColor Red"
