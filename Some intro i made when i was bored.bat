517955636a727ff85483e49e14226c6ea1aba708
Mr ZRX <158084979+pixelgunerd3YT@users.noreply.github.com>
2026-04-30 10:23:42 +0200

    Update Some intro i made when i was bored.bat

--- Some intro i made when i was bored.bat
+++ Some intro i made when i was bored.bat
@@ -7,6 +7,6 @@ powershell -command "Write-Host '	 |\  \    /  /|\  \|\   __  \|\  ___ \ |\   __
 powershell -command "Write-Host '	 \ \  \  /  / | \  \ \  \|\  \ \   __/|\ \  \|\  \   ' -ForegroundColor Red"
 powershell -command "Write-Host '		\ \  \/  / / \ \  \ \   ____\ \  \_|/_\ \   _  _\  ' -ForegroundColor Red"
 powershell -command "Write-Host '		 \ \    / /   \ \  \ \  \___|\ \  \_|\ \ \  \\  \| ' -ForegroundColor Red"
-powershell -command "Write-Host '			 \ \__/ /     \ \__\ \__\    \ \_______\ \__\\ _\ ' -ForegroundColor Red"
-powershell -command "Write-Host '			  \|__|/       \|__|\|__|     \|_______|\|__|\|__| ' -ForegroundColor Red"
+powershell -command "Write-Host '	    \ \__/ /     \ \__\ \__\    \ \_______\ \__\\ _\ ' -ForegroundColor Red"
+powershell -command "Write-Host '		   \|__|/       \|__|\|__|     \|_______|\|__|\|__| ' -ForegroundColor Red"
 echo.
